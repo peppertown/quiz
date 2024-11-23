7b22c378399b84644781f6e15b2727c26045f303
Dong Hyun <decidere@naver.com>
2024-11-24 00:31:52 +0900

    [Feat]: 단어 등록 컨트롤러 구현

--- controllers/word.controller.js
+++ controllers/word.controller.js
@@ -1,1 +1,18 @@
-import db from "../server";
+import db from "../server.js";
+
+// 단어 등록
+export const addWord = async (req, res) => {
+  try {
+    const words = req.body;
+    const sql = `INSERT INTO words (word, mean) VALUES(?,?)`;
+    for (let word of words) {
+      const values = [word.word, word.mean];
+      await db.execute(sql, values);
+    }
+    res
+      .status(200)
+      .json({ success: true, message: "단어 등록이 완료됐습니다." });
+  } catch (err) {
+    res.json(err);
+  }
+};
