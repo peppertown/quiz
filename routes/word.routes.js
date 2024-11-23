ef71d7a2311ed2f9c2ab1cecfd1f06ca8d5233e0
Dong Hyun <decidere@naver.com>
2024-11-24 00:32:08 +0900

    [Feat]: 단어 등록 컨트롤러 라우팅

--- routes/word.routes.js
+++ routes/word.routes.js
@@ -1,11 +1,13 @@
 import express from "express";
 const router = express.Router();
-
+import { addWord } from "../controllers/word.controller.js";
 /* 구현 기능 목록
 1. 단어 조회
-2. 단어 등록
 3. 단어 수정
 4. 단어 삭제
 5. 예문 생성 */
+
+// 단어 등록
+router.post("/", addWord);
 
 export default router;
