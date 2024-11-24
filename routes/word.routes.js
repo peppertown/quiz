0e305644177b4a41f9d11a36e7df3032faea9bb6
Dong Hyun <decidere@naver.com>
2024-11-24 19:31:10 +0900

    [Feat]: 단어 삭제 라우팅

--- routes/word.routes.js
+++ routes/word.routes.js
@@ -4,9 +4,9 @@ import {
   addWord,
   getWords,
   modifyWord,
+  deleteWord,
 } from "../controllers/word.controller.js";
 /* 구현 기능 목록
-4. 단어 삭제
 5. 예문 생성 */
 
 // 단어 등록
@@ -17,5 +17,8 @@ router.get("/", getWords);
 
 // 단어 수정
 router.put("/:id", modifyWord);
+
+// 단어 삭제
+router.delete("/:id", deleteWord);
 
 export default router;
