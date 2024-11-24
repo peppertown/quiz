bf01a0077607a88561680715d3e3c33b460dd7d7
Dong Hyun <decidere@naver.com>
2024-11-24 20:00:37 +0900

    [Feat]: app.js - sentence.routes 분리

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ import express from "express";
 import "dotenv/config.js";
 
 import wordRouter from "./routes/word.routes.js";
+import sentenceRouter from "./routes/sentence.routes.js";
 
 const app = express();
 app.use(express.json());
 
 app.use("/word", wordRouter);
+app.use("/sentence", sentenceRouter);
 
 const PORT_NUMBER = process.env.PORT_NUMBER;
 app.listen(PORT_NUMBER, () => {
