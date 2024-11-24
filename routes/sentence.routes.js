ac4b27bef833e662562d0a26ed9b2c7b00e16196
Dong Hyun <decidere@naver.com>
2024-11-24 20:31:02 +0900

    [Feat]: 예문 생성 라우팅

--- routes/sentence.routes.js
+++ routes/sentence.routes.js
@@ -1,9 +1,12 @@
 import express from "express";
 const router = express.Router();
+import { generateSenteces } from "../controllers/sentence.controller.js";
 
 /* 구현 기능 목록
-1. 예문 생성 
 2. 예문 조회
 */
+
+// 1. 예문 생성
+router.post("/", generateSenteces);
 
 export default router;
