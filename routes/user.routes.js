58cba9cb9063011e77158637b9046037379ce436
Dong Hyun <decidere@naver.com>
2024-11-25 23:03:01 +0900

    [Feat]: 로그인 라우팅

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,12 @@
 import express from "express";
 const router = express.Router();
 
-import { join } from "../controllers/user.controller";
+import { join, logIn } from "../controllers/user.controller";
 
 // 회원가입
 router.post("/join", join);
+
+// 로그인
+router.post("/login", logIn);
 
 export default router;
