1bc60ef21598afadaf9daf0c5ef789c2a3d33812
Dong Hyun <decidere@naver.com>
2024-11-25 23:02:56 +0900

    [Feat]: 로그인 컨트롤러 구현

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -14,3 +14,20 @@ export const join = async (req, res) => {
     return res.json(err);
   }
 };
+
+export const logIn = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const [result] = await db.execute("SELECT * FROM users WHERE email = ?", [
+      email,
+    ]);
+
+    if (result.length && result[0].password == password) {
+      res.json({ message: `${result[0].email}님 환영합니다.` });
+    } else {
+      res.json({ message: "등록되지 않은 계정입니다." });
+    }
+  } catch (err) {
+    res.json(err);
+  }
+};
