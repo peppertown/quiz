ecad798af17bea41ad605f3e8491270b875b1222
Dong Hyun <decidere@naver.com>
2024-12-01 04:42:19 +0900

    [Feat]: 노트 컨트롤러 구현 (단어장)

---
+++ controllers/note.controller.js
@@ -0,0 +1,14 @@
+import db from "../server.js";
+
+export const getNote = async (req, res) => {
+  let sql = `SELECT date FROM registered_date`;
+  const [days] = await db.execute(sql);
+
+  const datas = [];
+  for (let day of days) {
+    sql = `SELECT word, mean FROM words WHERE created_at = "${day.date}"`;
+    const [words] = await db.execute(sql);
+    datas.push({ date: day.date, words });
+  }
+  res.json(datas);
+};
