c24d5ebd3a4a1f64cc11b61b99e892cfe689e037
Dong Hyun <decidere@naver.com>
2024-12-01 04:58:39 +0900

    [Feat]: 단어 예문 조회 라우팅

--- routes/note.routes.js
+++ routes/note.routes.js
@@ -1,8 +1,11 @@
 import express from "express";
 const router = express.Router();
-import { getNote } from "../controllers/note.controller.js";
+import { getNote, getWordSentence } from "../controllers/note.controller.js";
 
 // 단어장 조회
 router.get("/", getNote);
+
+// 단어장 단어 예문 조회
+router.get("/:word", getWordSentence);
 
 export default router;
