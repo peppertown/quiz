a76ea7cff853583fd1002afc3db980f5e9406433
Dong Hyun <decidere@naver.com>
2024-12-02 01:39:16 +0900

    [Fix]: 단어장 조회 오류로 인한 코드 복구

--- controllers/note.controller.js
+++ controllers/note.controller.js
@@ -3,10 +3,16 @@ import db from "../server.js";
 // 단어장 조회
 export const getNote = async (req, res) => {
   try {
-    let sql = `SELECT r.date, w.word, w.mean FROM registered_date r JOIN words w ON r.date = w.created_at`;
-    const [dateWords] = await db.execute(sql);
+    let sql = `SELECT date FROM registered_date`;
+    const [days] = await db.execute(sql);
 
-    res.json(dateWords);
+    const datas = [];
+    for (let day of days) {
+      sql = `SELECT word, mean FROM words WHERE created_at = "${day.date}"`;
+      const [words] = await db.execute(sql);
+      datas.push({ date: day.date, words });
+    }
+    res.json(datas);
   } catch (err) {
     res.json(err);
   }
