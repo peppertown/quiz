dbfdd41cfddb07a65c192a94ac0d1c7a71a8d65b
Dong Hyun <decidere@naver.com>
2024-12-01 18:30:56 +0900

    [Feat]: 단어 예문 컨트롤러 응답데이터 수정

--- controllers/note.controller.js
+++ controllers/note.controller.js
@@ -19,5 +19,5 @@ export const getWordSentence = async (req, res) => {
   const { word } = req.params;
   const sql = `SELECT sentence, mean FROM sentence WHERE word="${word}"`;
   const [sentence] = await db.execute(sql);
-  res.json(sentence);
+  res.json({ word, sentence });
 };
