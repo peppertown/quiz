d253bdcf11590984a3967d08418f81f5e8547f6d
Dong Hyun <decidere@naver.com>
2024-12-01 23:35:30 +0900

    [Feat]: 단어장 조회 컨트롤러 쿼리 최적화

--- controllers/note.controller.js
+++ controllers/note.controller.js
@@ -3,16 +3,10 @@ import db from "../server.js";
 // 단어장 조회
 export const getNote = async (req, res) => {
   try {
-    let sql = `SELECT date FROM registered_date`;
-    const [days] = await db.execute(sql);
+    let sql = `SELECT r.date, w.word, w.mean FROM registered_date r JOIN words w ON r.date = w.created_at`;
+    const [dateWords] = await db.execute(sql);
 
-    const datas = [];
-    for (let day of days) {
-      sql = `SELECT word, mean FROM words WHERE created_at = "${day.date}"`;
-      const [words] = await db.execute(sql);
-      datas.push({ date: day.date, words });
-    }
-    res.json(datas);
+    res.json(dateWords);
   } catch (err) {
     res.json(err);
   }
