ef53cf3decd50517f0ccc9e40f0c104cc4237ff7
Dong Hyun <decidere@naver.com>
2024-12-02 02:00:27 +0900

    [Feat]: 스크랩 컨트롤러 라우팅

--- routes/scrap.routes.js
+++ routes/scrap.routes.js
@@ -1,4 +1,8 @@
 import express from "express";
 const router = express.Router();
+import { handleScraps } from "../controllers/scrap.controller.js";
+
+// 스크랩 추가/제거
+router.post("/:id", handleScraps);
 
 export default router;
