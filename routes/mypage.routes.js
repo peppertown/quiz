b1024d53267f618d3307724601b38625007d4a3b
Dong Hyun <decidere@naver.com>
2024-12-01 23:34:27 +0900

    [Feat]: 마이페이지 라우팅

--- routes/mypage.routes.js
+++ routes/mypage.routes.js
@@ -1,4 +1,8 @@
 import express from "express";
 const router = express.Router();
+import { getMypage } from "../controllers/mypage.controller.js";
+
+// 마이페이지 조회
+router.get("/", getMypage);
 
 export default router;
