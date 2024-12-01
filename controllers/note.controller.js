31452877de7053f9aaa94ec3760538ee805020f6
Dong Hyun <decidere@naver.com>
2024-12-01 19:40:54 +0900

    [Feat]: 단어장 컨트롤러 예외처리

--- controllers/note.controller.js
+++ controllers/note.controller.js
@@ -2,22 +2,30 @@ import db from "../server.js";
 
 // 단어장 조회
 export const getNote = async (req, res) => {
-  let sql = `SELECT date FROM registered_date`;
-  const [days] = await db.execute(sql);
+  try {
+    let sql = `SELECT date FROM registered_date`;
+    const [days] = await db.execute(sql);
 
-  const datas = [];
-  for (let day of days) {
-    sql = `SELECT word, mean FROM words WHERE created_at = "${day.date}"`;
-    const [words] = await db.execute(sql);
-    datas.push({ date: day.date, words });
+    const datas = [];
+    for (let day of days) {
+      sql = `SELECT word, mean FROM words WHERE created_at = "${day.date}"`;
+      const [words] = await db.execute(sql);
+      datas.push({ date: day.date, words });
+    }
+    res.json(datas);
+  } catch (err) {
+    res.json(err);
   }
-  res.json(datas);
 };
 
 // 단어의 예문 조회
 export const getWordSentence = async (req, res) => {
-  const { word } = req.params;
-  const sql = `SELECT sentence, mean FROM sentence WHERE word="${word}"`;
-  const [sentence] = await db.execute(sql);
-  res.json({ word, sentence });
+  try {
+    const { word } = req.params;
+    const sql = `SELECT sentence, mean FROM sentence WHERE word="${word}"`;
+    const [sentence] = await db.execute(sql);
+    res.json({ word, sentence });
+  } catch (err) {
+    res.json(err);
+  }
 };
