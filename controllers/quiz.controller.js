f0ae4ab95d6bf63c446a7a713a98ab98c4c2d62e
Dong Hyun <decidere@naver.com>
2024-12-03 02:50:41 +0900

    [Feat]: 퀴즈 리스트 조회 컨트롤러 구현

---
+++ controllers/quiz.controller.js
@@ -0,0 +1,20 @@
+import db from "../server.js";
+
+// 퀴즈 리스트 조회
+export const getQuizzes = async (req, res) => {
+  try {
+    let sql = `SELECT word,mean FROM words ORDER BY RAND() LIMIT 5`;
+    const [wordsForQuiz] = await db.execute(sql);
+
+    sql = `SELECT w.word, s.sentence, s.mean FROM words w JOIN sentence s ON w.word = s.word ORDER BY RAND() LIMIT 5`;
+    const [sentencesForQuiz] = await db.execute(sql);
+
+    const quizzes = {
+      wordQuiz: wordsForQuiz,
+      senetenceQuiz: sentencesForQuiz,
+    };
+    res.json(quizzes);
+  } catch (err) {
+    res.json(err);
+  }
+};
