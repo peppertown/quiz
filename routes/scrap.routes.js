cfe3f9c7d41c74903c78c47e85167fe7f1755082
Dong Hyun <decidere@naver.com>
2024-12-02 17:39:34 +0900

    [Feat]: 스크랩 제거 라우팅

--- routes/scrap.routes.js
+++ routes/scrap.routes.js
@@ -1,10 +1,18 @@
 import express from "express";
 const router = express.Router();
-import { handleScraps, getScraps } from "../controllers/scrap.controller.js";
+import {
+  handleScraps,
+  getScraps,
+  deleteScrap,
+} from "../controllers/scrap.controller.js";
 
 // 스크랩 추가/제거
 router.post("/:id", handleScraps);
 
+// 스크랩 목록 조회
 router.get("/", getScraps);
+
+// 스크랩 제거
+router.delete("/:id", deleteScrap);
 
 export default router;
