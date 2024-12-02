4c222bb39bbea20cf55413705b464a6fc21207eb
Dong Hyun <decidere@naver.com>
2024-12-03 02:51:32 +0900

    [Feat]: 퀴즈 리스트 생성 라우팅

--- routes/quiz.routes.js
+++ routes/quiz.routes.js
@@ -1,4 +1,7 @@
 import express from "express";
 const router = express.Router();
+import { getQuizzes } from "../controllers/quiz.controller.js";
+
+router.get("/", getQuizzes);
 
 export default router;
