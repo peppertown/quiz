a11d6fd396b8129ed6cff2e1c33c439f26c9f945
Dong Hyun <decidere@naver.com>
2024-12-02 17:29:31 +0900

    [Feat]: 스크랩 목록 조회 라우팅

--- routes/scrap.routes.js
+++ routes/scrap.routes.js
@@ -1,8 +1,10 @@
 import express from "express";
 const router = express.Router();
-import { handleScraps } from "../controllers/scrap.controller.js";
+import { handleScraps, getScraps } from "../controllers/scrap.controller.js";
 
 // 스크랩 추가/제거
 router.post("/:id", handleScraps);
+
+router.get("/", getScraps);
 
 export default router;
