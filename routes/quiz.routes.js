d76890246a8f057442d1870b783a30c0ccdbbd78
Dong Hyun <decidere@naver.com>
2024-12-03 07:14:12 +0900

    [Feat]: 날짜별 퀴즈 생성 라우팅

--- routes/quiz.routes.js
+++ routes/quiz.routes.js
@@ -1,14 +1,18 @@
 import express from "express";
 const router = express.Router();
 import {
-  getQuizzes,
+  getAllQuizzes,
   handleWrongWords,
+  getDateQuiz,
 } from "../controllers/quiz.controller.js";
 
 // 퀴즈 리스트 생성
-router.get("/all", getQuizzes);
+router.get("/all", getAllQuizzes);
 
 // 퀴즈 피드백
 router.post("/", handleWrongWords);
+
+// 날짜별 퀴즈 리스트 생성
+router.get("/", getDateQuiz);
 
 export default router;
