c49f522d314baa3112d02340183f2b3b5c6a87a4
Dong Hyun <decidere@naver.com>
2024-12-03 03:17:37 +0900

    [Feat]: 퀴즈 리스트 생성 응답데이터에 단어 id 값 추가

--- controllers/quiz.controller.js
+++ controllers/quiz.controller.js
@@ -3,10 +3,10 @@ import db from "../server.js";
 // 퀴즈 리스트 생성
 export const getQuizzes = async (req, res) => {
   try {
-    let sql = `SELECT word,mean FROM words ORDER BY RAND() LIMIT 5`;
+    let sql = `SELECT id, word,mean FROM words ORDER BY RAND() LIMIT 5`;
     const [wordsForQuiz] = await db.execute(sql);
 
-    sql = `SELECT w.word, s.sentence, s.mean FROM words w JOIN sentence s ON w.word = s.word ORDER BY RAND() LIMIT 5`;
+    sql = `SELECT w.id, w.word, s.sentence, s.mean FROM words w JOIN sentence s ON w.word = s.word ORDER BY RAND() LIMIT 5`;
     const [sentencesForQuiz] = await db.execute(sql);
 
     const quizzes = {
