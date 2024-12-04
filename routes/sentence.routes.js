90c753127c851e69ed2be3f36195e978e12b50af
Dong Hyun <decidere@naver.com>
2024-12-04 22:09:05 +0900

    [Feat]: 예문 재생성 라우팅

--- routes/sentence.routes.js
+++ routes/sentence.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 import {
   generateSenteces,
   getSentences,
+  regenerateSentence,
 } from "../controllers/sentence.controller.js";
 
 // 1. 예문 생성
@@ -10,5 +11,8 @@ router.post("/", generateSenteces);
 
 // 2. 예문 조회
 router.get("/", getSentences);
+
+// 3. 예문 재생성
+router.post("/regenerate", regenerateSentence);
 
 export default router;
