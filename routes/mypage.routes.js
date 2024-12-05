56ac15025179463d91f2a9671f32ea4422fa468b
Dong Hyun <decidere@naver.com>
2024-12-05 18:06:20 +0900

    [Feat]: 복습 단어 조회 라우팅

--- routes/mypage.routes.js
+++ routes/mypage.routes.js
@@ -1,8 +1,11 @@
 import express from "express";
 const router = express.Router();
-import { getMypage } from "../controllers/mypage.controller.js";
+import { getMypage, getWordHistory } from "../controllers/mypage.controller.js";
 
 // 마이페이지 조회
 router.get("/", getMypage);
+
+// 복습 단어 조회
+router.get("/history", getWordHistory);
 
 export default router;
